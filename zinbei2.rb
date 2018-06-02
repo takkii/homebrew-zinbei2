436a0ad1068b83c5b8813b55957b736b53280a2c
Takayuki Kamiyama <karuma.reason@gmail.com>
2018-06-02 17:04:35 +0900

    Update.

--- zinbei2.rb
+++ zinbei2.rb
@@ -2,6 +2,7 @@ require "formula"
 
 class Zinbei2 < Formula
   homepage "http://nyasocom.site/takkii/zinbei2"
+  url "http://www.nyasocom.site/takkii/zinbei2/-/archive/master/zinbei2-master.tar.gz"
   head "http://nyasocom.site/takkii/zinbei2.git"
   version "2.0.1"
   def install
