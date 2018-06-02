c1aba623e4edb65beea211cad8dd9ed99b31b36e
Takayuki Kamiyama <karuma.reason@gmail.com>
2018-06-02 16:32:39 +0900

    HEAD file default.

---
+++ zinbei2.rb
@@ -0,0 +1,8 @@
+require "formula"
+
+class Zinbei2 < Formula
+  homepage "http://nyasocom.site/takkii/zinbei2"
+  url "http://nyasocom.site/takkii/zinbei2/-/archive/master/zinbei2-master.tar.gz"
+  head "http://nyasocom.site/takkii/zinbei2.git"
+  version "2.0.1"
+end
