b45e4da851cd8c7319ee6b117598da119d642a5a
Takayuki Kamiyama <karuma.reason@gmail.com>
2018-06-02 18:46:40 +0900

    Update.

--- zinbei2.rb
+++ zinbei2.rb
@@ -4,7 +4,7 @@ class Zinbei2 < Formula
   
   homepage "https://github.com/takkii/zinbei2"
   url "https://github.com/takkii/zinbei2/archive/2.0.1_01.tar.gz"
-  sha256 "487384518ec41afb53d86f524d659b0155444619b89b39b1a06f5599ca186f01"
+  sha256 "6675f56e0a4897a0ac65402833e18811970a416caab86c15e419d70e76f3a527"
   head "https://github.com/takkii/zinbei2.git"
   version "2.0.1"
 
