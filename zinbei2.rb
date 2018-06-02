273ad3a119d827ee4af60ed683fcd03b5bea3bd5
Takayuki Kamiyama <karuma.reason@gmail.com>
2018-06-02 17:01:56 +0900

    Update.

--- zinbei2.rb
+++ zinbei2.rb
@@ -4,4 +4,7 @@ class Zinbei2 < Formula
   homepage "http://nyasocom.site/takkii/zinbei2"
   head "http://nyasocom.site/takkii/zinbei2.git"
   version "2.0.1"
+  def install
+   bin.install
+  end
 end
