9bf9d5159042d94ff04bfe3cbfd09cd936efc57a
Takayuki Kamiyama <karuma.reason@gmail.com>
2018-08-05 13:51:44 +0900

    Update.

--- zinbei2.rb
+++ zinbei2.rb
@@ -3,10 +3,10 @@ require "formula"
 class Zinbei2 < Formula
   
   homepage "https://github.com/takkii/zinbei2"
-  url "https://github.com/takkii/zinbei2/archive/2.0.1_13.tar.gz"
-  sha256 "c72eeaafe28e7dd9b1e317415aab5e0c3c38abe8f19c8616619809c6de530be0"
+  url "https://github.com/takkii/zinbei2/archive/2.0.2_0.tar.gz"
+  #sha256 "c72eeaafe28e7dd9b1e317415aab5e0c3c38abe8f19c8616619809c6de530be0"
   head "https://github.com/takkii/zinbei2.git"
-  version "2.0.1"
+  version "2.0.2"
 
   def install
    bin.install Dir['bin/*']
