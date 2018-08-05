bd87e1a218716218c19443ee13da7b5c143f5425
Takayuki Kamiyama <karuma.reason@gmail.com>
2018-08-05 14:59:48 +0900

    Update.

--- zinbei2.rb
+++ zinbei2.rb
@@ -3,13 +3,12 @@ require "formula"
 class Zinbei2 < Formula
   
   homepage "https://github.com/takkii/zinbei2"
-  url "https://github.com/takkii/zinbei2/archive/2.0.2_2.tar.gz"
-  sha256 "f9cedec7062276fa72a8a6211d1d80c69a639398d8e5b2739d48591681c1b252"
+  url "https://github.com/takkii/zinbei2/archive/2.0.2_3.tar.gz"
+  # sha256 ""
   head "https://github.com/takkii/zinbei2.git"
   version "2.0.2"
 
   def install
    bin.install Dir['bin/*']
-   bin.install Dir['image/tweet_icon.gif']
   end
 end
